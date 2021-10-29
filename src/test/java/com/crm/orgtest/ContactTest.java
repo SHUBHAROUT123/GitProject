e1af132feb6678ef6ed430c45677bfa230708e25
SUBHAROUT123 <subharout017@gmail.com>
2021-10-29 11:51:54 +0530

    Framework is done

---
+++ src/test/java/com/crm/orgtest/ContactTest.java
@@ -0,0 +1,17 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createContactTest()
+{
+	System.out.println("execute createcontacttest");
+}
+@Test
+public void editContactTest()
+{    
+	System.out.println("execute editcontact");
+}
+
+}
